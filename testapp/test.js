1266290b1eb267f2f117e21a57529c93d89a98bc
Ester Hugosson <eh225bx@student.lnu.se>
2024-09-30 15:15:46 +0200

    tests

--- testapp/test.js
+++ testapp/test.js
@@ -5,7 +5,11 @@ let value = [22, 25, 28]
 
 const wrapper = new StorageWrapper()
 
-wrapper.setDataToLocalStorage(key, value)
+//wrapper.setDataToLocalStorage(key, value)
 
 
 console.log(wrapper.getDataFromLocalStorage(key))
+
+wrapper.removeData(key)
+
+wrapper.setDataToLocalStorage('This', value)
