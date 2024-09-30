d1268b6cb4a1cf2cb8c29b369818bbab0f3cb298
Ester Hugosson <eh225bx@student.lnu.se>
2024-09-30 17:46:13 +0200

    Toggle between storage and start of handling expiration times

--- storageWrapper/storageWrapper.js
+++ storageWrapper/storageWrapper.js
@@ -1,22 +1,58 @@
 export class StorageWrapper {
 
-    // Required key and value to set 
+    // Default local storage
     constructor(storageType = 'local') {
 
         this.storage = storageType === 'local' ? localStorage : sessionStorage
 
     }
 
-    setDataToLocalStorage(key, value) {
+    //Toggle between local and session storage
+    toggleStorage() {
 
-        if(key !== '' && value !== '') {
+        if(this.storage === localStorage) {
+            this.storage = sessionStorage
+            this.storageType = 'session'
+            console.log('Now using Sessionstorage')
+        } else if(this.storage === sessionStorage) {
+            this.storage = localStorage
+            this.storageType = 'local'
+            console.log('Now using localstorage')
+        }
+
+    }
+
+
+    //Save data from storage
+    setData(key, value, options = {}) {
+
+        /* if(key !== '' && value !== '') {
         this.storage.setItem(key, JSON.stringify(value))
         } else {
             console.error(' Key or value is empty ')
+        } */
+
+        if(key === '') {
+            console.error('Key is empty')
+        } else if(value === '') {
+            console.error('Value is empty')
+        } else {
+
+            const data = {
+                value: JSON.stringify(value),
+                expiry: Date.now() + options
+            }
+
+            this.storage.setItem(key, JSON.stringify(data))
+            console.log(`${key} was saved succesfully in ${this.storageType}storage`)
+
         }
+
+        
     }
 
-    getDataFromLocalStorage(key) {
+    //get specifik data from storage
+    getData(key) {
 
         let value = JSON.parse(this.storage.getItem(key))
 
@@ -28,9 +64,10 @@ export class StorageWrapper {
         
     }
 
+    // remove certain key and its value from storage
     removeData(key) {
 
-        if(this.getDataFromLocalStorage(key) === null ) {
+        if(this.getData(key) === null ) {
 
             console.log('This data does not exist')
 
@@ -38,6 +75,11 @@ export class StorageWrapper {
         
         this.storage.removeItem(key)
 
+    }
+
+    //remove all data from storage
+    clear() {
+        this.storage.clear()
     }
 
 }
