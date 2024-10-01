fa247fbc7e1c93e9700475aab519fb92869af817
Ester Hugosson <eh225bx@student.lnu.se>
2024-10-01 17:17:16 +0200

    Eventlistener for test buttons

--- testapp/test.js
+++ testapp/test.js
@@ -15,3 +15,38 @@ const getButton = document.getElementById('getSubmitButton')
 const deleteButton = document.getElementById('deleteSubmitButton')
 const toggleButton = document.getElementById('switchSubmitButton')
 const clearButton = document.getElementById('clearSubmitButton')
+
+// Set data
+setButton.addEventListener('click', () => {
+    const key = keyInput.value
+    const value = valueInput.value
+    const ttl = ttlInput.value ? parseInt(ttlInput.value) : null
+
+    wrapper.setData(key, value, ttl)
+
+})
+
+//Get data
+getButton.addEventListener('click', () => {
+
+    const key = keyInput.value
+
+    const data = wrapper.getData(key)
+    resultdiv.innerHTML = data ? `Data for key "${key}": ${data}` : `No data found for key "${key}".`
+    
+})
+
+//Remove data
+deleteButton.addEventListener('click', () => {
+    
+})
+
+//Toggle storage 
+toggleButton.addEventListener('click', () => {
+    
+})
+
+//Clear storage
+clearButton.addEventListener('click', () => {
+    
+})
