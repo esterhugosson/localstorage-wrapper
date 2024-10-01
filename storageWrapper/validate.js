dc6831128e11be4beaa45bb921b99d0deaf53ed8
Ester Hugosson <eh225bx@student.lnu.se>
2024-10-01 13:56:01 +0200

    Validation of key and value

--- storageWrapper/validate.js
+++ storageWrapper/validate.js
@@ -5,11 +5,18 @@
 
 export class Validator {
 
+    //Key must be type string and not empty 
     isValidKey(key) {
 
+        return typeof key === 'string' && key.trim() !== ''
+        //returns true when both is true 
     }
 
+    //value must be defiened and not null 
     isValidValue(value) {
+
+        //returns true when both is true
+        return value !== undefined && value !== null
 
     
     }
