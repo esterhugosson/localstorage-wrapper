d2413665a40ec73c90cfc1d957a18b0486e1bbf4
Ester Hugosson <eh225bx@student.lnu.se>
2024-10-01 15:46:05 +0200

    Validation and error messages

--- storageWrapper/storageWrapper.js
+++ storageWrapper/storageWrapper.js
@@ -27,7 +27,7 @@ export class StorageWrapper {
         } else if(this.storage === sessionStorage) {
             this.storage = localStorage
             this.storageType = 'local'
-            console.log('Now using localstorage')
+            console.log('Now using Localstorage')
         }
 
     }
@@ -44,10 +44,13 @@ export class StorageWrapper {
         if(!this.validator.isValidValue(value)) {
             throw new Error('Invalid value. Please enter a valid value.')
         }
+        if(ttl !== null && !this.validator.isTTLvalid(ttl)) {
+            throw new Error('Invalid expiration. Please enter a valid time.')
+        }
 
         const data = {
             value: JSON.stringify(value),
-            expiry: ttl ? new Date().getTime() + ttl : null 
+            expiry: ttl ? new Date().getTime() + ttl * 1000 : null 
         }
 
         this.storage.setItem(key, JSON.stringify(data))
@@ -62,53 +65,58 @@ export class StorageWrapper {
 
 
         if(!this.validator.isValidKey(key)) {
-            throw new Error('Invalid key. Get data failed.')
-        }
-
-
-
-        const value = this.storage.getItem(key)
-
-        //If the value does not exist, return null
-        if(!value) {
-            console.error('This data does not exist')
+            console.error('Invalid key. Get data failed.')
             return null
         }
 
-        const valueParsed = JSON.parse(value)
 
-        const now = new Date()
+        const storedData = this.storage.getItem(key)
 
-        if(now.getTime() > valueParsed.expiry) {
-            this.removeData(this.key)
-            console.log('This data is expired')
+        //If the value does not exist for given key
+        if(!storedData) {
+            console.error(`Data does not exist in ${this.storageType} for given key: ${key}`)
             return null
         }
 
-        console.log(`Sucessfully retirived the data from key: ${key}, data: ${valueParsed.value}`)
+        const dataParsed = JSON.parse(storedData)
 
 
-            
-        return valueParsed.value
+        //Expiration controll (if data has an expiration time)
+        if(dataParsed.expiry && new Date().getTime() > dataParsed.expiry) {
+            this.removeData(key)
+            console.log(`Data for key ${key} is expired and been removed.`)
+            return null
+        }
+
+
+        console.log(`Sucessfully retirived the data from key: ${key}`)
+        return JSON.parse(dataParsed.value)
         
     }
 
     // remove certain key and its value from storage
     removeData(key) {
 
-        if(this.getData(key) === null ) {
+        if(!this.validator.isValidKey(key)) {
+            console.error('Invalid key. Data removal failed.')
+            return
+        }
 
-            console.log('This data does not exist')
-
+        if(!this.storage.getItem(key)) {
+            console.error(`Data for ${key} does not exist. Removal failed.`)
+            return
         }
         
+
         this.storage.removeItem(key)
+        console.log(` Data for key: ${key} has been sucessfully removed from ${this.storageType}storage`)
 
     }
 
     //remove all data from storage
     clear() {
         this.storage.clear()
+        console.log(`All data has been cleared from ${this.storageType}storage`)
     }
 
 }
