1cc8fac416822736d1763146f1f7706cee27ed50
Ester Hugosson <eh225bx@student.lnu.se>
2024-10-01 13:21:01 +0200

    seperate validation responsibility in its own class

---
+++ storageWrapper/validate.js
@@ -0,0 +1,18 @@
+/**
+ * Validation class for storage wrapper. 
+ * @author Ester Hugosson 
+ */
+
+export class Validator {
+
+    isValidKey(key) {
+
+    }
+
+    isValidValue(value) {
+
+    
+    }
+
+
+}
