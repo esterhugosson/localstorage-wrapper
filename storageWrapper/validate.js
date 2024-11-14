be4a8b11243a57695985b6a96bb783b0bc63f2e5
Ester Hugosson <eh225bx@student.lnu.se>
2024-11-14 10:54:28 +0000

    All validation in this class

--- storageWrapper/validate.js
+++ storageWrapper/validate.js
@@ -1,29 +1,47 @@
 /**
  * Validation class for storage wrapper. 
+ * @version 1.0.1
  * @author Ester Hugosson 
  */
 
 export class Validator {
 
-    //Key must be type string and not empty 
     isValidKey(key) {
 
         return typeof key === 'string' && key.trim() !== ''
-        //returns true when both is true 
+        
     }
 
-    //value must be defiened and not null 
+    // value must be defiened and not null 
     isValidValue(value) {
 
-        //returns true when all is true
+        
         return value !== undefined && value !== null && value.trim() !== ''
 
     }
 
-    // is TIME TO LIVE, ttl, a number bigger than 0
-    isTTLvalid(ttl) {
+    // is expiration a number, greater than zero
+    isValidExpiration(expiration) {
 
-        return typeof ttl === 'number' && ttl > 0
+        return typeof expiration === 'number' && expiration > 0 &&  !isNaN(expiration)
+
+    }
+
+    validateAll(key, value, expiration) {
+
+        if (!this.isValidKey(key)) {
+            throw new Error('Invalid key: must be a non-empty string. ')
+        }
+
+        if (!this.isValidValue(value)) {
+            throw new Error('Invalid value: cannot be undefined or null. ')
+        }
+
+        if (!this.isValidExpiration(expiration)) {
+            throw new Error('Invalid expiration: must be a positive number. ')
+        }
+
+        return true
 
     }
 
