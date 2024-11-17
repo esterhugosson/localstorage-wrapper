485e268a56842ffe79ddc5d50ef821128682daaa
Ester Hugosson <eh225bx@student.lnu.se>
2024-11-17 15:29:31 +0000

    Validation refactoring

--- storageWrapper/validate.js
+++ storageWrapper/validate.js
@@ -6,44 +6,41 @@
 
 export class Validator {
 
-    isValidKey(key) {
-
-        return typeof key === 'string' && key.trim() !== ''
-        
-    }
-
-    // value must be defiened and not null 
-    isValidValue(value) {
-
-        
-        return value !== undefined && value !== null && value.trim() !== ''
-
-    }
-
-    // is expiration a number, greater than zero
-    isValidExpiration(expiration) {
-
-        return typeof expiration === 'number' && expiration > 0 &&  !isNaN(expiration)
-
-    }
-
-    validateAll(key, value, expiration) {
-
-        if (!this.isValidKey(key)) {
+    validateKey(key) {
+        if (!this.#isValidKey(key)) {
             throw new Error('Invalid key: must be a non-empty string. ')
         }
-
-        if (!this.isValidValue(value)) {
-            throw new Error('Invalid value: cannot be undefined or null. ')
-        }
-
-        if (!this.isValidExpiration(expiration)) {
-            throw new Error('Invalid expiration: must be a positive number. ')
-        }
-
         return true
-
     }
 
+    #isValidKey(key) {
+        return typeof key === 'string' && key.trim() !== ''
+    }
+
+
+
+    validateValue(value) {
+        if (!this.#isValidValue(value)) {
+            throw new Error('Invalid value: cannot be undefined or null. ')
+        }
+        return true
+    }
+
+    #isValidValue(value) {
+        return value !== undefined && value !== null && value !== ''
+    }
+
+
+
+    validateExpiration(expiration) {
+        if (!this.#isValidExpiration(expiration)) {
+            throw new Error('Invalid expiration: must be a positive number. ')
+        }
+        return true     
+    }
+
+    #isValidExpiration(expiration) {
+        return typeof expiration === 'number' && expiration > 0
+    }
 
 }
